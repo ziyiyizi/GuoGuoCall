dc6f8edb84a684806ef90d0807036d443f7fb09c
ziyi <xiajinlei@foxmail.com>
2018-03-21 22:11:32 +0800

    add more material designs and change the layout

---
+++ app/src/main/java/com/example/ziyi/contactscall/ContactAdapter.java
@@ -0,0 +1,10 @@
+package com.example.ziyi.contactscall;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ziyi on 2018/3/21.
+ */
+
+public class ContactAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/example/ziyi/contactscall/Contacts.java
@@ -0,0 +1,8 @@
+package com.example.ziyi.contactscall;
+
+/**
+ * Created by ziyi on 2018/3/21.
+ */
+
+public class Contacts {
+}
